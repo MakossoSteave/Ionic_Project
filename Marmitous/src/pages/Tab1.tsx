029da50951585e17845b8c8b2c8077310da75009
AlexisPriser <priseralexis78@gmail.com>
2021-04-01 21:55:48 +0200

    ptite modifs

--- Marmitous/src/pages/Tab1.tsx
+++ Marmitous/src/pages/Tab1.tsx
@@ -1,146 +1,89 @@
-import ExploreContainer from '../components/ExploreContainer';
-import { IonButton, IonContent, IonItem, IonLabel,IonPage, IonList, IonListHeader, IonThumbnail, IonImg, IonGrid, IonRow, IonCol} from '@ionic/react';
+import ExploreContainer from "../components/ExploreContainer";
+import {
+  IonButton,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonList,
+  IonListHeader,
+  IonThumbnail,
+  IonImg,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from "@ionic/react";
 
-
-import './Tab1.css';
-
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
   type Item = {
     src: string;
     text: string;
   };
-  const items: Item[] = [{ src: "https://www.toutlevin.com/img/1309ccef4a22d5ef95eb4c6dd6a2d60a004740003000-960.jpg", text: 'Salade niçoise' }];
-  
+  const items: Item[] = [];
+
+  const img = {
+    src:
+      "https://www.toutlevin.com/img/1309ccef4a22d5ef95eb4c6dd6a2d60a004740003000-960.jpg",
+    text: "Salade niçoise",
+  };
+
+  for (let i = 0; i < 10; i++) {
+    items.push(img);
+  }
+
   return (
     <IonPage>
+      <IonContent>
+        <IonListHeader>
+          <IonLabel>Marmitous</IonLabel>
+        </IonListHeader>
 
-    <IonContent>
- 
-    <IonListHeader>
-      <IonLabel>Marmitous</IonLabel>
-    </IonListHeader>
+        <iframe
+          src="https://www.youtube.com/embed/Gvzfwuw1PZE"
+          width="200"
+          height="150"
+        ></iframe>
 
-   
-   
-    <IonList>
-      <IonListHeader lines="inset">
-        <IonLabel>Recent</IonLabel>
-      </IonListHeader>
-      <IonItem lines="none">
-        
         <IonList>
-      {items.map((image, i) => (
-        <IonItem key={i}>
-          <IonThumbnail slot="start">
-            <IonImg src={image.src} />
-          </IonThumbnail>
-          <IonLabel>{image.text}</IonLabel>
-        </IonItem>
-      ))}
-    </IonList>
-      </IonItem>
-    </IonList>
+          <IonListHeader lines="inset">
+            <IonLabel>Recent</IonLabel>
+          </IonListHeader>
+          <IonItem lines="none">
+            <IonList>
+              {items.map((image, i) => (
+                <IonItem key={i}>
+                  <IonThumbnail slot="start">
+                    <IonImg src={image.src} />
+                  </IonThumbnail>
+                  <IonLabel>{image.text}</IonLabel>
+                </IonItem>
+              ))}
+            </IonList>
+          </IonItem>
+        </IonList>
 
-    <IonList>
-      <IonListHeader lines="inset">
-        <IonLabel>Liste des plats</IonLabel>
-      </IonListHeader>
-   
-     
-    </IonList>
-    <IonGrid>
-      <IonRow>
-      {items.map((image, i) => (
-        <IonItem key={i}>
-        <IonCol> <IonImg src={image.src} />
-        <IonLabel>interessant</IonLabel>
-
-        </IonCol>
-        <IonCol> <IonImg src={image.src} />
-        <IonLabel>interessant</IonLabel>
-</IonCol>
-      
-        </IonItem>
-        ))}
-
-      </IonRow>
-      <IonRow>
-      {items.map((image, i) => (
-        <IonItem key={i}>
-        <IonCol> <IonImg src={image.src} />
-        <IonLabel>interessant</IonLabel>
-</IonCol>
-        <IonCol> <IonImg src={image.src} />
-        <IonLabel>interessant</IonLabel>
-</IonCol>
-      
-        </IonItem>
-        ))}
-
-      </IonRow>
-      <IonRow>
-      {items.map((image, i) => (
-        <IonItem key={i}>
-        <IonCol> <IonImg src={image.src} />
-        <IonLabel>interessant</IonLabel>
-</IonCol>
-        <IonCol> <IonImg src={image.src} />
-        <IonLabel>interessant</IonLabel>
-</IonCol>
-      
-        </IonItem>
-        ))}
-
-      </IonRow>
-      <IonRow>
-      {items.map((image, i) => (
-        <IonItem key={i}>
-        <IonCol> <IonImg src={image.src} />
-        <IonLabel>interessant</IonLabel>
-</IonCol>
-        <IonCol> <IonImg src={image.src} />
-        <IonLabel>interessant</IonLabel>
-</IonCol>
-      
-        </IonItem>
-        ))}
-
-      </IonRow>
-      <IonRow>
-      {items.map((image, i) => (
-        <IonItem key={i}>
-        <IonCol> <IonImg src={image.src} />
-        <IonLabel>interessant</IonLabel>
-</IonCol>
-        <IonCol> <IonImg src={image.src} />
-        <IonLabel>interessant</IonLabel>
-</IonCol>
-      
-        </IonItem>
-        ))}
-
-      </IonRow>
-      <IonRow>
-      {items.map((image, i) => (
-        <IonItem key={i}>
-        <IonCol> <IonImg src={image.src} />
-        <IonLabel>interessant</IonLabel>
-
-        </IonCol>
-        <IonCol> <IonImg src={image.src} />
-        <IonLabel>interessant</IonLabel>
-</IonCol>
-      
-        </IonItem>
-        ))}
-
-      </IonRow>
-    </IonGrid>
-  </IonContent>
-  </IonPage>
-
-  
+        <IonList>
+          <IonListHeader lines="inset">
+            <IonLabel>Liste des plats</IonLabel>
+          </IonListHeader>
+        </IonList>
+        <IonGrid>
+          <IonRow>
+            {items.map((image, i) => (
+              <IonItem key={i}>
+                <IonCol>
+                  {" "}
+                  <IonImg src={image.src} />
+                  <IonLabel>{image.text}</IonLabel>
+                </IonCol>
+              </IonItem>
+            ))}
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
   );
 };
 
